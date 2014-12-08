aa272ba1607ca6574680a37c9c6d91cadf0bbc33
Artem Meshcheryakov <messteno@gmail.com>
2014-12-09 01:47:04 +0300

    Fix styles

--- mesteno/viewsets.py
+++ mesteno/viewsets.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework import permissions
 from .serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (permissions.IsAdminUser, )
